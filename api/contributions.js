32eec12be38ad457cd0dc21b0c9085a6815abd10
ArditXhaferi <ardit@starlabspro.com>
2023-01-29 21:21:29 +0100

    Vercel fixes

--- api/contributions.js
+++ api/contributions.js
@@ -2,6 +2,28 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", async (req, res) => {
+    const githubRequest = (url, name) => {
+        return new Promise((resolve, reject) => {
+            fetch(url.replace("$name$", name))
+                .then((response) => response.json())
+                .then((data) => {
+                    let MyDate = new Date();
+                    let MyDateString;
+    
+                    MyDateString = MyDate.getFullYear() + '-' + ('0' + (MyDate.getMonth() + 1)).slice(-2) + "-" + ('0' + MyDate.getDate()).slice(-2);
+    
+                    data.contributions.forEach((contribution, index) => {
+                        if (contribution.date == MyDateString) {
+                            resolve(data.contributions.slice(index, index + 30))
+                        }
+                    });
+                })
+                .catch((error) => {
+                    reject(error);
+                });
+        });
+    };
+
     try {
         let contributions = await githubRequest(
             `https://github-contributions.vercel.app/api/v1/$name$`,
@@ -14,29 +36,5 @@ router.get("/", async (req, res) => {
         return res.status(500).send("Server error");
     }
 });
-
-const githubRequest = (url, name) => {
-    console.log(name)
-    return new Promise((resolve, reject) => {
-        fetch(url.replace("$name$", name))
-            .then((response) => response.json())
-            .then((data) => {
-                console.log(data);
-                let MyDate = new Date();
-                let MyDateString;
-
-                MyDateString = MyDate.getFullYear() + '-' + ('0' + (MyDate.getMonth() + 1)).slice(-2) + "-" + ('0' + MyDate.getDate()).slice(-2);
-
-                data.contributions.forEach((contribution, index) => {
-                    if (contribution.date == MyDateString) {
-                        resolve(data.contributions.slice(index, index + 30))
-                    }
-                });
-            })
-            .catch((error) => {
-                reject(error);
-            });
-    });
-};
 
 module.exports = router;
